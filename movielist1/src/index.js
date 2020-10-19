27a4a7510cb93676af27bb8fd0f7353ced99af1c
natalia <00120319@uca.edu.sv>
2020-10-18 18:15:25 -0600

    Add: Creation Project Structure
    Add: Creation Project Structure

---
+++ movielist1/src/index.js
@@ -0,0 +1,10 @@
+//console.log("movielist");
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+    return <h1>movieList</h1>
+}
+
+//Con React Dom se renderiza para mostrar el componente APP
+ReactDom.render(<App/>, document.getElementById('root'));
